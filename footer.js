76f77efd184724ebb5babf0675063a714f86ad1d
Omkp84 <155177180+Omkp84@users.noreply.github.com>
2024-05-26 10:19:44 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="attribution">
+      Coded by <a href="https://github.com/Omkp84/">Om Patel</a>
+      
+    </footer>
+  );
+};
+
+export default Footer;
